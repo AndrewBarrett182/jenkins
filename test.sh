0962c97f23dac5eaf357a581c45f0e451a1b9e72
Andrew Barrett <andrew.barrett@ecs.co.uk>
2021-10-21 15:37:20 +0100

    fix

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sh "cat index.html | grep '<p><small>Deployed by Jenkins job: ${BUILD_NUMBER}</small></p>'"
+cat index.html | grep '<p><small>Deployed by Jenkins job: ${BUILD_NUMBER}</small></p>'
